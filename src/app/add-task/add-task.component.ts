e36aa5b532a11862262b755eb72819919f08240f
Wilson Britto <mikebwilson94@gmail.com>
2019-02-02 03:34:13 +0530

    Added Validation for Add task

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavBarService } from '../Services/NavBar/nav-bar.service';
+import { FormBuilder, Validators, FormGroup, FormControl, AbstractControl } from '@angular/forms';
+import { AbstractClassPart } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-add-task',
@@ -7,11 +9,43 @@ import { NavBarService } from '../Services/NavBar/nav-bar.service';
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+  Priority: number;
+  UsrFrm: FormGroup;
+  constructor(private nav: NavBarService, private fb: FormBuilder) {
+    this.Priority = 0;
+    this.UsrFrm = fb.group({
+      Task: ['', [Validators.required]],
+      Priority: [],
+      ParentTask: [],
+      StartDate: ['', [Validators.required]],
+      EndDate: ['', [Validators.required]],
+    });
+  }
 
-  constructor(private nav: NavBarService) { }
+  get TaskCtrl() {
+    return this.UsrFrm.get('Task');
+  }
+  get PriorityCtrl() {
+    return this.UsrFrm.get('Priority');
+  }
+  get ParentTaskCtrl() {
+    return this.UsrFrm.get('ParentTask');
+  }
+  get StartDateCtrl() {
+    return this.UsrFrm.get('StartDate');
+  }
+  get EndDateCtrl() {
+    return this.UsrFrm.get('EndDate');
+  }
+
+
+  onSubmit(): void {
+    console.log(this.UsrFrm);
+  }
 
   ngOnInit() {
     this.nav.show();
+    this.PriorityCtrl.valueChanges.subscribe(d => this.Priority = d);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -20,7 +22,9 @@ import { ViewTaskComponent } from './view-task/view-task.component';
       { path: "", redirectTo: "/ViewTask", pathMatch: 'full' },
       { path: "AddTask", component: AddTaskComponent },
       { path: "ViewTask", component: ViewTaskComponent }
-    ])
+    ]),
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
