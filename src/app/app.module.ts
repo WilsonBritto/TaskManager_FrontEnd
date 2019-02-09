ca84971b5bf9238d793b42b281d32f0a5f92fe05
Wilson Britto <mikebwilson94@gmail.com>
2019-02-09 23:15:54 +0530

    Added data service and injected HttpClientModule and added it in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -27,7 +28,8 @@ import { UpdateTaskComponent } from './update-task/update-task.component';
       { path: "UpdateTask", component: UpdateTaskComponent }
     ]),
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
