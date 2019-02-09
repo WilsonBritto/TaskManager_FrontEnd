4c71c035c25d90fb59339b7105e0f64bd4c1e533
Wilson Britto <mikebwilson94@gmail.com>
2019-02-10 00:10:51 +0530

    Inherited DataService in Taskservice

--- src/app/Services/Data/Task/task.service.ts
+++ src/app/Services/Data/Task/task.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TaskService {
+export class TaskService extends DataService {
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super("http://localhost:8186/api/task", http);
+  }
 }
