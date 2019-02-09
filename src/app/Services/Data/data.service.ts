c30e04eece2b8e83cee4a495a5cb50685b9e7a38
Wilson Britto <mikebwilson94@gmail.com>
2019-02-10 00:02:28 +0530

    Added crud methods in DataService

--- src/app/Services/Data/data.service.ts
+++ src/app/Services/Data/data.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ITask } from 'src/app/Shared/Models/itask';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,20 @@ import { HttpClient } from '@angular/common/http';
 export class DataService {
 
   constructor(private url: string, private http: HttpClient) { }
+
+  getAll(): Observable<ITask[]> {
+    return this.http.get<ITask[]>(this.url);
+  }
+  get(id: number): Observable<ITask> {
+    return this.http.get<ITask>(this.url + "/" + id);
+  }
+  create(task: ITask): Observable<ITask> {
+    return this.http.post<ITask>(this.url, JSON.stringify(task));
+  }
+  update(id: number, task: ITask): Observable<ITask> {
+    return this.http.put<ITask>(this.url + "/" + id, JSON.stringify(task));
+  }
+  delete(id: number): Observable<any> {
+    return this.http.delete(this.url + "/" + id);
+  }
 }
